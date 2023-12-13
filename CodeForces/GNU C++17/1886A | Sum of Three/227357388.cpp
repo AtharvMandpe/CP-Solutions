cb1b2215c26ee05e6938f7a9b997856d6bd49711
Kiyotaka <111036572+AtharvMandpe@users.noreply.github.com>
2023-12-13 15:18:41 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1886A | Sum of Three/227357388.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<7 || n==9){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+            if(n%3==0){
+                cout << "1 4 " << n-5 << endl;
+            }
+            else{
+                cout << "1 2 " << n-3 << endl;
+            }
+        }
+    }
+ 
+    return 0;
+}
