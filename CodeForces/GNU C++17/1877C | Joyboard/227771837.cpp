b926e92df407b88023f694d1e4b206a092013830
Kiyotaka <111036572+AtharvMandpe@users.noreply.github.com>
2023-12-13 15:18:34 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1877C | Joyboard/227771837.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        if(k==1){
+            cout << 1 << endl;
+        }
+        else if(k==2){
+            cout << max(0, m/n - 1) + min(m,n) << endl;
+        }
+        else if(k==3){
+            cout << max(0, (m-n+1) - (m/n)) << endl;
+        }
+        else if(k>3){
+            cout << 0 << endl;
+        }
+    }
+    return 0;
+}
